4c5c40dfeee9d7020f6568f955ef77188997bf76
1105213969 <1105213969@qq.com>
2019-08-17 23:22:58 +0800

    程序入口界面

---
+++ src/com/zsw/snake/Main.java
@@ -0,0 +1,48 @@
+package com.zsw.snake;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * @program: Snake
+ * @description: 游戏入口
+ * @author: shengweiz
+ * @create: 2019-08-17 22:33
+ **/
+public class Main extends JFrame {
+
+    private static ImageIcon bg = new ImageIcon(Main.class.getResource("bg.jpg"));//背景图片
+
+    public Main() {
+        super("贪吃蛇");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(bg.getIconWidth() + 30, bg.getIconHeight() + 50);
+        setLocation(200, 100);
+    }
+
+    public static void main(String[] args) {
+        Main entry = new Main();
+        Container c = entry.getContentPane();
+        c.setLayout(null);
+        JLabel bgLable = new JLabel(bg);
+        bgLable.setBounds(0, 0, bg.getIconWidth(),
+                bg.getIconHeight());//bg图片标签
+        JButton startBtn = new JButton("开始游戏");//开始按钮
+        JButton quitBtn = new JButton("退出游戏");//推出按钮
+        startBtn.setBounds(300, 150, 100, 50);
+        quitBtn.setBounds(300, 220, 100, 50);
+        c.add(startBtn);
+        c.add(quitBtn);
+        c.add(bgLable);
+
+        startBtn.addActionListener((ActionEvent e)->{
+            //开始游戏
+        });
+        quitBtn.addActionListener((ActionEvent e)->{
+            //退出游戏
+            System.exit(0);
+        });
+        entry.setVisible(true);
+    }
+}
